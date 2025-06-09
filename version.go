387a0834eec4663ecb7adabf5dadfbdc25a79e34
M. Price <26305909+Zaptross@users.noreply.github.com>
2025-06-09 13:32:41 +1000

    Feat/log version (#6)
    * feat(version): add logging for application version
    
    * refactor(ip): move ip funcs to own file

---
+++ version.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	dhv "github.com/zaptross/godohuver"
+)
+
+func getVersion() string {
+	dat, err := os.ReadFile("/etc/program-version")
+
+	if err != nil {
+		slog.Warn("Failed to read version file", "error", err)
+		dat = []byte("unknown")
+	}
+
+	version := string(dat)
+	versionMessage := fmt.Sprintf("Version: %s", version)
+
+	semver, err := dhv.ExtractSemver(version)
+	if err == nil {
+		latest, err := dhv.GetLatestImage("zaptross/countula")
+
+		if err == nil {
+			if semver != latest.Tag {
+				versionMessage = fmt.Sprintf("%s\nNew version available: %s", versionMessage, latest.Tag)
+			}
+		}
+	}
+
+	return versionMessage
+}
